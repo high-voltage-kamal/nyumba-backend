2361979a3fd84fdc610a418b60b4f3c6e05660df
high-voltage-kamal <pesazamimi@gmail.com>
2026-05-21 09:25:45 +0300

    Update wsgi.py

--- nyumba/wsgi.py
+++ nyumba/wsgi.py
@@ -1,5 +1,19 @@
 import os
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nyumba.settings")
+
 application = get_wsgi_application()
+
+# Auto-create superuser on first run
+try:
+    from apps.accounts.models import User
+    if not User.objects.filter(phone_number="+254700000000").exists():
+        User.objects.create_superuser(
+            phone_number="+254700000000",
+            password="NyumbaAdmin2026!",
+            full_name="Admin",
+        )
+except Exception:
+    pass
